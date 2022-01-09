1af46c6de046ec28919b1a8219e4ef27fd0dbf93
agqyhapwj <agqyhapwj555@gmail.com>
2022-01-09 12:09:51 +0800

    2022 0109寫一個自定義的function 把程式碼包起來，要用時在呼叫TestCode()

--- javascript/main.js
+++ javascript/main.js
@@ -1,9 +1,22 @@
 // 瀏覽器的控制台，就是「console.log」的列印語法
 /* 單或雙引號裡的(' , ")皆為字串 */
 
-// 變數命名：英文開頭 + 數字 + 底線，不能以數字開頭，javascript的關鍵字不能設定，英文大小寫混用
-// es5版本 var
-// var 宣告全域變數
+// 呼叫來使用
+TestCode();
+
+    // 日期函數 Date 產生每年都會自動更新的 Date↓
+    var year = new Date().getFullYear();
+
+    // 年份變數↑
+    // var Year = "2022";
+    // 由html 顯示資料
+    document.getElementById('Year').textContent = year;
+
+// 自定義一個 function name(參數引數或空自) {程式碼}
+function TestCode() {
+    // 變數命名：英文開頭 + 數字 + 底線，不能以數字開頭，javascript的關鍵字不能設定，英文大小寫混用
+    // es5版本 var
+    // var 宣告全域變數
     var one;
     var ONE;
     var One;
@@ -16,14 +29,6 @@
     console.log(one);
     console.log(ONE);
     console.log("02");
-
-    // 日期函數 Date 產生每年都會自動更新的 Date↓
-    var year = new Date().getFullYear();
-
-    // 年份變數↑
-    // var Year = "2022";
-    // 由html 顯示資料
-    document.getElementById('Year').textContent = year;
 
     // 單一條件判斷 if 三種寫法
     var NumBer = 12;
@@ -42,4 +47,7 @@
         console.log("NumBer比較小");
     else
         console.log("NumBer 比較大");
+}
+
+    
     
