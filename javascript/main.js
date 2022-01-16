c98cccf1754be66b9474f4749a6e75ee9aa050fd
agqyhapwj <agqyhapwj555@gmail.com>
2022-01-16 14:34:04 +0800

    20220116 用陣列與迴圈產生七個li+它的class

--- javascript/main.js
+++ javascript/main.js
@@ -31,3 +31,34 @@ function CopyRightContent () {
     // 3:在被選取的元素(之前)插入內容 語法:before() 置入被選取元素標籤之前
     Address.before(div);
 }
+
+// 新增 SmallNav 的內容
+SmallNavCon();
+function SmallNavCon() {
+    // 新增標籤 ul與li
+    let ul = document.createElement('ul');
+    // let li = document.createElement('li');
+
+    // 在Smallnav內新增，宣告變數
+    let SN = document.getElementById('SmallNav');
+
+    // 在 SN 建立 ul 框架，給予 id 名稱
+    SN.appendChild(ul);
+    // 賦予屬性 id or class
+    ul.setAttribute("id", "PhoneNav");
+
+    let PhoneNav = document.getElementById('PhoneNav');
+
+    let NavClass = ['About', 'VerticalLine', 'Shop', 'VerticalLine', 'LocalMap', 'VerticalLine', 'Message']
+    
+    // 新增標籤時，程式的運作方式為：先新增標籤，賦予屬性，再指定新增的外框架(此為一個迴圈)
+    // 迴圈內的數字 7 是因為陣列產生，故要隨著陣列值的數量而自動增減
+    for (let i = 0; i < NavClass.length; i++) {
+        let li = document.createElement('li');
+        // 新增屬性
+        li.setAttribute("class", NavClass[i]);
+        // ul 內新增 li 清單
+        PhoneNav.appendChild(li);
+    }
+
+}
