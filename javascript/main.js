d97866eb2f2ee0fe6d3d69962a7541ae47418baf
agqyhapwj <agqyhapwj555@gmail.com>
2022-01-16 17:09:51 +0800

    2022 0116 做了很多的設定

--- javascript/main.js
+++ javascript/main.js
@@ -75,8 +75,21 @@ function SmallNavCon() {
 }
 
 // 手機裝置選單
-MenuNav();
+// MenuNav();
 function MenuNav() {
     let MenuNav = document.getElementById('MenuNav');
     MenuNav.style.display = "none";
+}
+
+// PhoneMenushow();
+// menu手機版按鈕
+function PhoneMenushow() {
+    let Menu = document.getElementsByClassName('MenuBtn');
+    // 顯示手機版選單內容
+    Menu.style.display = "block";
+}
+
+Pageload();
+function Pageload() {
+    $('header').load("../web/header.html");
 }
