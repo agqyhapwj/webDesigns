ea2f05e07a88412ba8143652a52797d2902a1111
agqyhapwj <agqyhapwj555@gmail.com>
2022-01-16 10:20:57 +0800

    2022 0116 建立CopyRightContent()

--- javascript/main.js
+++ javascript/main.js
@@ -3,4 +3,17 @@ TimeYear();
 function TimeYear() {
     var year = new Date().getFullYear();
     document.getElementById('Year').textContent = year;
-};
+}
+
+// 新增html的標籤與內容
+CopyRightContent();
+function CopyRightContent () {
+    // 宣告變數在「html」內新增 div 標籤
+    let div = document.createElement('div');
+
+    // 宣告變數在哪個「html」標籤之內新增 div
+    let Footer = document.getElementById('Footer');
+
+    // 在footer內，新增 div 子元素
+    Footer.appendChild(div);
+}
