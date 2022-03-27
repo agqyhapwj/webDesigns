2e5102ca2ef6dea81b2907586be66bf27f3b1cfe
agqyhapwj <agqyhapwj555@gmail.com>
2022-03-27 09:57:01 +0800

    2022 0327 測試新動畫循環

--- javascript - 複製/lib/Banner.js
+++ javascript - 複製/lib/Banner.js
@@ -6,17 +6,25 @@ let current = 0;
 
 // 方法 宣告變數，將執行方法直接寫入變數中
 let ChangeImage = function(num) {
-    // 判斷式條件 -- 索引編號 + 自訂參數值 >=0 && 索引編號 + 自訂參數 < 圖片陣列的長度成立時
-    if(current + num >= 0 && current + num < images.length) {
-        // 索引變數 = 索引變數 + 自訂參數 current = current + num
-        current += num;
-        // 更變 圖片的連結檔案
-        document.getElementById('Bannerimg').src = images[current];
-    } 
-    else {
-        // 這段 else 是後來加的，才會一直循環；否則執行 0 1 2 3 4 後就停止了
-        current = -1;
-    }
+if(current + num >=0 && current + num < images.length) {
+    current = current + num;
+}else if (current + num < images.length){
+    current = images.length;
+}
+
+    // // 判斷式條件 -- 索引編號 + 自訂參數值 >=0 && 索引編號 + 自訂參數 < 圖片陣列的長度成立時
+    // if(current + num >= 0 && current + num < images.length) {
+    //     // 索引變數 = 索引變數 + 自訂參數 current = current + num
+    //     current += num;
+    //     // 更變 圖片的連結檔案
+    //     document.getElementById('Bannerimg').src = images[current];
+    // } 
+    // else {
+    //     // 這段 else 是後來加的，才會一直循環；否則執行 0 1 2 3 4 後就停止了
+    //     current = -1;
+    // }
+console.log(current);
+
 }
 
 // 按鈕執行 方法
@@ -29,10 +37,10 @@ document.getElementById('next').onclick = function() {
 }
 
 // 自動播放 3000毫秒 = 三秒
-let Timer = setInterval(() => {
-    if ( current < images.length ) {
-        ChangeImage(1);
-        // 用console.log(current)，印出它執行的次數
-        console.log(current);
-    }
-},3000);
+// let Timer = setInterval(() => {
+//     if ( current < images.length ) {
+//         ChangeImage(1);
+//         // 用console.log(current)，印出它執行的次數
+//         console.log(current);
+//     }
+// },3000);
